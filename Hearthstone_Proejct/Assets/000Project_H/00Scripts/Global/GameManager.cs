423740b99f124e6d712198e73297ae0dc96cceac
CVA11Ha11A <dad0221@naver.com>
2024-02-16 03:36:17 +0900

    feat : LobbyScene

---
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Global/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{       // 싱글턴
+
+    private GameManager instance;
+    public GameManager Instance
+    {
+        get 
+        { 
+            if (instance == null || instance == default)
+            {
+                GameObject manager = new GameObject("GameManager");
+                manager.AddComponent<GameManager>();
+            }
+            return instance; 
+        }
+    }
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+}       // GameManager Class
---
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Lobby/Buttons/CollectionButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionButton : MonoBehaviour
+{
+    private Image collectionImage;
+
+    private void Awake()
+    {
+        collectionImage = this.GetComponent<Image>();
+    }       // Awake()
+
+    public void ClickCollectionButton()
+    {
+        // 카드가 들어있는 UI들을 오픈하여 덱을 만들 수 있음
+
+    }       // ClickCollectionButton()
+
+}       // CollectionButton Class
