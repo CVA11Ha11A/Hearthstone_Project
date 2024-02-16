ba6c6eff77e87e8cb275849a0bf68632111f68f4
CVA11Ha11A <dad0221@naver.com>
2024-02-16 13:37:48 +0900

    feat : LobbyCanvasControllerScript

---
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Lobby/LobbyCanvasController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyCanvasController : MonoBehaviour
+{    
+    private Vector3 originV3 = default;
+    private Vector3 moveV3 = default;
+
+    private void Awake()
+    {        
+        originV3 = this.transform.position;
+        moveV3 = new Vector3(300f, 300f, -300f);
+        this.transform.position = moveV3;
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+}       // LobbyCanvasController Class
