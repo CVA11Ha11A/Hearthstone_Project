1dc4bd6b6cd78770d30922977b07c84db718a676
CVA11Ha11A <dad0221@naver.com>
2024-02-20 20:36:27 +0900

    feat : MinionBaseScript, MinionObject

---
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Card/Minions/Nor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Norgannon : MinionCard
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+}       // Norgannon ClassEnd
