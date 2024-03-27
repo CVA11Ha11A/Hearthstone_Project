5665a35a252187a79c79e054e84d169871f4d95a
CVA11Ha11A <dad0221@naver.com>
2024-03-28 04:36:20 +0900

    feat : CollectionCanvasCardInteractionClass_Click,Drag Interface work fix : RayShootPosition, MainCameraProjection

--- Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/CollectionCanvasCardInteraction.cs
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/CollectionCanvasCardInteraction.cs
@@ -1,43 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CollectionCanvasCardInteraction : MonoBehaviour, IDragHandler, IPointerUpHandler, IBeginDragHandler , IPointerDownHandler
+public class CollectionCanvasCardInteraction : MonoBehaviour, IDragHandler, IPointerUpHandler, IBeginDragHandler, IPointerDownHandler
 {       // DeckBuild State일떄 켜주어서 플레이어와 카드의 상호작용을 할 수 있게 해주는 클래스
-
-    RaycastHit hitObj = default;
-    public int targetLayer = default;
-    public LayerMask layerMask = default;
+    
+    public LayerMask cardLayerMask = default;
+    public LayerMask deckCardListLayerMask = default;
     public Card lastChoiceCard = null;
+
+    private float rayMaxDistance = default;
+    private Ray ray = default;
+    private RaycastHit hitInfo = default;
+
+    private GameObject instanceCard = null;    
 
     private void Awake()
     {
-        targetLayer = LayerMask.GetMask("Card");
-        layerMask = LayerMask.GetMask("Card");
+        rayMaxDistance = 300f;
+        cardLayerMask = 1 << 6;        
+        deckCardListLayerMask = 1 << 7;
         this.enabled = false;
     }
-    void Start()
-    {
-        Gizmos.color = Color.red;
-    }
 
+    private void Start()
+    {
+        if (instanceCard == null)
+        {   // ! 30오브젝트 생성이 30 ~ 100개정도로 인스턴스 예상이됨 이정도면 풀링 사용안하는것이 더 나을듯?
+            Transform parentTargetTras = this.transform.GetChild(1).GetChild(2);    // ! target == PageObj
+            instanceCard = Instantiate(CardManager.Instance.cardPrefab, Vector3.zero, Quaternion.identity, parentTargetTras);
+            instanceCard.transform.localScale = new Vector3(25f, 25f, 25f);
+            instanceCard.gameObject.SetActive(false);
+        }
+    }
     #region Interface Methods
 
     #region LEGACY
 
 
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
-    {   // 클릭시 호출        
-        Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
-      Input.mousePosition.y, -Camera.main.transform.position.z));
+    {   // 클릭시 호출
 
-        if (Physics.Raycast(point, Vector3.forward, out hitObj, 300f, layerMask))
-        {            
-            lastChoiceCard = hitObj.collider.GetComponent<Card>();
+        // 마우스 스크린 좌표 얻기
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        // 마우스 스크린 좌표를 월드 좌표로 변환
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y,
+            Camera.main.nearClipPlane));
+
+        if (Physics.Raycast(mouseWorldPosition, Vector3.forward, out hitInfo, rayMaxDistance, cardLayerMask))
+        {
+            lastChoiceCard = hitInfo.collider.GetComponent<Card>();
         }
-        
 
     }       // OnPointerDown()
 
@@ -45,16 +62,51 @@ public class CollectionCanvasCardInteraction : MonoBehaviour, IDragHandler, IPoi
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {   // 드래그중 호출
+        // TODO : 현제 개체가 따라다니는 중이라면 계속 따라다니도록
+        if (instanceCard.gameObject.activeSelf == true)
+        {
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y,
+                Camera.main.nearClipPlane));
+            mouseWorldPosition.z = -1f;
+            instanceCard.transform.position = mouseWorldPosition;
+        }
+        else { /*PASS*/ }
 
-    }
+    }       // IDragHandler.OnDrag()
 
     void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
     {   // 클릭 땔때 호출
+        // TODO : Prefab이 켜져있으며 땐곳이 덱 리스트라면 현재 카드추가
+        //          아니라면 카드추가하지 않고 Prefab setActive = Flase
+        if (instanceCard.gameObject.activeSelf == true)
+        {
+            // 마우스 스크린 좌표 얻기
+            Vector3 mouseScreenPosition = Input.mousePosition;
+            // 마우스 스크린 좌표를 월드 좌표로 변환
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y,
+                Camera.main.nearClipPlane));
 
+            if (Physics.Raycast(mouseWorldPosition, Vector3.forward, rayMaxDistance, deckCardListLayerMask))
+            {
+                   // TODO : 제작해야함
+            }            
+            Destroy(instanceCard.GetComponent<Card>());
+            instanceCard.gameObject.SetActive(false);
+        }
+        lastChoiceCard = null;
     }
 
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {   // 드래그 하는 순간        
+        // TODO : lastChoiceCard가 != null 이라면
+        // Card Prefab이 보이게 되며 해당 개체가 마우스를 따라다님
+
+        if (lastChoiceCard != null)
+        {
+            instanceCard.gameObject.SetActive(true);
+            instanceCard.AddComponent(lastChoiceCard.GetType());
+        }
 
     }       // OnBeginDrag()
 
