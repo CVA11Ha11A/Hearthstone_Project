c29556921de4079e68aa986b0db895fca3301d4c
CVA11Ha11A <dad0221@naver.com>
2024-03-30 00:38:51 +0900

    feat : DeckInCardObject Swap fix : FeildData -> ClassData

--- Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/CollectionDeckCardList.cs
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/CollectionDeckCardList.cs
@@ -15,9 +15,9 @@ public class CollectionDeckCardList : MonoBehaviour
         GameManager.Instance.GetTopParent(this.transform).GetComponent<CollectionCanvasController>().deckCardListRoot = this;
 
         cardList = new GameObject[this.transform.childCount];
-        for (int i = 0; i < cardList.Length; i++) 
+        for (int i = 0; i < cardList.Length; i++)
         {
-            cardList[i] = this.transform.GetChild(i).gameObject;        
+            cardList[i] = this.transform.GetChild(i).gameObject;
         }
         SetActiveFlaseToChilds();
 
@@ -25,7 +25,7 @@ public class CollectionDeckCardList : MonoBehaviour
 
     private void SetActiveFlaseToChilds()
     {
-        for (int i = 0; i < cardList.Length; i++) 
+        for (int i = 0; i < cardList.Length; i++)
         {
             cardList[i].gameObject.SetActive(false);
         }
@@ -34,15 +34,56 @@ public class CollectionDeckCardList : MonoBehaviour
 
     public void AddToCard(Card addCard_)
     {
+        #region CardSetting
         cardList[currentIndex].SetActive(true);
 
         deckInCardRoot = cardList[currentIndex].GetComponent<DeckInCard>();
-        deckInCardRoot.CardName = addCard_.cardName;
-        deckInCardRoot.CardCost = addCard_.cost;
-        deckInCardRoot.SetCardId(addCard_.cardId);
-        deckInCardRoot.cardImageRoot.sprite = addCard_.cardImage.sprite;
+        #region LEGACY
+        //deckInCardRoot.CardName = addCard_.cardName;
+        //deckInCardRoot.CardCost = addCard_.cost;
+        //deckInCardRoot.SetCardId(addCard_.cardId);
+        //deckInCardRoot.cardImageRoot.sprite = addCard_.cardImage.sprite;
+        #endregion LEGACY
 
-        currentIndex++;
+        deckInCardRoot.datas.cardId = addCard_.cardId;
+        deckInCardRoot.datas.cardCost = addCard_.cost;
+        deckInCardRoot.datas.cardName = addCard_.cardName;
+        deckInCardRoot.datas.cardSprite = addCard_.cardImage.sprite;        
+
+        #endregion CardSetting
+
+        #region SortCardList
+        DeckInCard cardIRoot = default;
+        DeckInCard cardJRoot = default;
+        for (int i = 0; i < currentIndex; i++)
+        {  
+            cardIRoot = cardList[i].GetComponent<DeckInCard>();
+
+            for (int j = i + 1; j <= currentIndex; j++)
+            {                
+
+                cardJRoot = cardList[j].GetComponent<DeckInCard>();
+                if (cardIRoot.datas.cardCost > cardJRoot.datas.cardCost)
+                {
+                    DeckInCardData tempRoot = cardIRoot.datas;
+                    cardIRoot.datas = cardJRoot.datas;
+                    cardJRoot.datas = tempRoot;
+                }
+            }
+        }
+        #endregion SortCardList
+
+        #region UpdataUI
+        for (int i = 0; i <= currentIndex; i++)
+        {
+            cardList[i].GetComponent<DeckInCard>().UpdateUI();
+        }
+        #endregion UpdataUI
+
+        currentIndex++;        
+
     }       // AddToCard()
+
+
 
 }       // ClassEnd
--- Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/DeckInCard.cs
+++ Hearthstone_Proejct/Assets/000Project_H/00Scripts/Collection/DeckInCard.cs
@@ -1,51 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class DeckInCardData
+{
+    public Sprite cardSprite = default;
+    public CardID cardId = default;
+    public string cardName = default;
+    public int cardCost = default;
+    
+}
+
 public class DeckInCard : MonoBehaviour
 {
     public Image cardImageRoot = null;
     private TextMeshProUGUI costTextRoot = null;
     private TextMeshProUGUI cardNameRoot = null;
-    private CardID cardId = default;                // 중요한 것이기에 Private로 감춤
-    
+    public DeckInCardData datas = default;
 
-    public string CardName
-    {
-        get { return this.CardName; }
-        set
-        {
-            if(this.CardName != value)
-            {
-                this.CardName = value;
-            }
-            cardNameRoot.text = this.CardName;
-        }
-    }
+    #region LEGACY
+    //private CardID cardId = default;                // 중요한 것이기에 Private로 감춤
+    //
+    //private string cardName = default;
+    //
+    //public string CardName
+    //{
+    //    get { return this.cardName; }
+    //    set
+    //    {
+    //        if (this.cardName != value)
+    //        {
+    //            this.cardName = value;
+    //        }
+    //        cardNameRoot.text = this.cardName;
+    //    }
+    //}
+    //
+    //private int cardCost = default;
+    //public int CardCost
+    //{
+    //    get
+    //    {
+    //        return cardCost;
+    //    }
+    //    set
+    //    {
+    //        if (cardCost != value)
+    //        {
+    //            cardCost = value;
+    //        }
+    //        costTextRoot.text = CardCost.ToString();
+    //    }
+    //}
+    #endregion LEGACY
 
-        
 
-    private int cardCost = default;
-    public int CardCost
-    {
-        get
-        {
-            return cardCost;
-        }
-        set
-        {            
-            if(cardCost != value)
-            {
-                cardCost = value;
-            }
-            costTextRoot.text = CardCost.ToString();
-        }
-    }
 
     private void Awake()
     {
+        datas = new DeckInCardData();
         cardImageRoot = this.transform.GetChild(0).GetChild(0).GetComponent<Image>();
         costTextRoot = this.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
         cardNameRoot = this.transform.GetChild(0).GetChild(2).GetComponent<TextMeshProUGUI>();
@@ -57,11 +73,17 @@ public class DeckInCard : MonoBehaviour
         
     }
 
-    
+    [Obsolete("필드 데이터 -> 클래스 참조 데이터")]
     public void SetCardId(CardID cardId_)
-    {
-        this.cardId = cardId_;
+    {   // 2024.03.29 수정 [Obsolete 추가]
+        //this.cardId = cardId_;
     }       // SetCardId()
     
+    public void UpdateUI()
+    {
+        costTextRoot.text = datas.cardCost.ToString();
+        cardNameRoot.text = datas.cardName.ToString();
+        cardImageRoot.sprite = datas.cardSprite;
+    }
 
 }       // ClassEnd
